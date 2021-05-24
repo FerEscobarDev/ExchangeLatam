f5aa02c9e24e60760f9685ec18c0f0ed51dc02eb
keishmen <57463087+keishmen@users.noreply.github.com>
2021-05-24 16:53:05 -0500

    update layout dashboard
    Se separa el navbar, sidebar y footer del layout dashboard para mejor manejo de los componentes

---
+++ resources/views/includes/footer_dash.blade.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="{{route('company.index')}}">
+                        ExchangeLatam
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('company.about')}}">
+                        Acerca de nosotros
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('company.faq')}}">
+                        Faq
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, Todos los derechos reservados.
+        </div>
+    </div>
+</footer>
---
+++ resources/views/includes/sidebar_dash.blade.php
@@ -0,0 +1,66 @@
+            <div class="sidebar" data-color="green" data-background-color="black"
+                data-image="{{asset('img_web/city-sidebar.jpg')}}">
+                <div class="logo">
+                    <a href="{{route('company.index')}}" class="simple-text logo-mini">
+                        <img src="{{asset('img_web/logo-mini.png')}}" class="img-fluid">
+                    </a>
+                    <a href="{{route('user.dashboard')}}" class="simple-text logo-normal">
+                        ExchangeLatam
+                    </a>
+                </div>
+                <div class="sidebar-wrapper">
+                    <div class="user">
+                        <div class="photo">
+                            @isset(auth()->user->picture)
+                                <img src="{{asset('storage/'.auth()->user->picture)}}">
+                            @else
+                                <img src="{{asset('img_web/default-avatar.png')}}">
+                            @endisset
+                        </div>
+                        <div class="user-info">
+                            <a data-toggle="collapse" href="#collapseProfile" class="username">
+                                <span>
+                                    {{ strtok(auth()->user()->name, " ")." ".strtok(auth()->user()->lastname, " ") }}
+                                    <b class="caret"></b>
+                                </span>
+                            </a>
+                            <div class="collapse" id="collapseProfile">
+                                <ul class="nav">        
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">
+                                            @isset(auth()->user->picture)
+                                                <span class="sidebar-mini"> CF </span>
+                                                <span class="sidebar-normal"> Cambiar Foto </span>
+                                            @else
+                                                <span class="sidebar-mini"> SF </span>
+                                                <span class="sidebar-normal"> Subir Foto </span>
+                                            @endisset
+                                        </a>
+                                    </li>{{-- 
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">
+                                            <span class="sidebar-mini"> EP </span>
+                                            <span class="sidebar-normal"> Edit Profile </span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">
+                                            <span class="sidebar-mini"> S </span>
+                                            <span class="sidebar-normal"> Settings </span>
+                                        </a>
+                                    </li> --}}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="nav">
+                        <li class="nav-item {{ request()->routeIs('user.dashboard') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{route('user.dashboard')}}">
+                                <i class="material-icons">dashboard</i>
+                                <p> Dashboard </p>
+                            </a>
+                        </li>                   
+                    </ul>
+                </div>
+                <div class="sidebar-background"></div>
+            </div>
