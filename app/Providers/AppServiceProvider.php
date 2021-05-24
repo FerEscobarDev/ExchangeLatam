888c50b9ee0054670ede6864be140b9f6199e920
keishmen <57463087+keishmen@users.noreply.github.com>
2021-05-24 16:50:26 -0500

    Personalización emails resetPassword and verifyEmail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        VerifyEmail::$toMailCallback = function($notifiable, $verificationUrl)
+        {
+            return(new MailMessage)
+                ->subject('Verificación de email - ExchangeLatam')
+                ->view('emails.verifyEmail',[
+                    'name' => $notifiable->name,
+                    'lastname' => $notifiable->lastname,
+                    'url' => $verificationUrl
+                ]);
+        };
+
+        ResetPassword::$toMailCallback = function($notifiable, $token)
+        {
+            return(new MailMessage)
+                ->subject('Restablecer contraseña - ExchangeLatam')
+                ->view('emails.resetPasswordEmail', [
+                    'name' => $notifiable->name,
+                    'lastname' => $notifiable->lastname,
+                    'email' => $notifiable->email,
+                    'token' => $token
+                ]);
+        };
     }
 }
