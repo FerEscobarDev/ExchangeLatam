deb52911b621d3cd4efd2f8e43814f9d13840441
keishmen <57463087+keishmen@users.noreply.github.com>
2021-05-24 16:45:35 -0500

    Update auth.php

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'failed' => 'El correo o la contraseña no coinciden con nuestros registros.',
     'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
 
 ];
+    
