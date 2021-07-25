962c6549770586d61747bb2503f55d3970a8ce34
keishmen <57463087+keishmen@users.noreply.github.com>
2021-07-24 23:55:50 -0500

    Update RouteUsers.php

--- app/Http/Middleware/RouteUsers.php
+++ app/Http/Middleware/RouteUsers.php
@@ -18,9 +18,9 @@ class RouteUsers
     public function handle(Request $request, Closure $next)
     {
         $route = $request->server->all()['HTTP_HOST'];
-        dd($route.' '.env('APP_URL'));
+        //dd($route.' '.env('APP_URL'));
 
-        if($route == env('APP_URL'))
+        if($route == 'https://www.exchangelatam.com')
         {
             return $next($request);
         }else
