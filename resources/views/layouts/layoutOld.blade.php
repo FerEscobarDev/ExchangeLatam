8a6e5f1b4cee1cb509b0a71980c7b1d16f99e256
keishmen <57463087+keishmen@users.noreply.github.com>
2021-08-03 13:50:36 -0500

    Install Tag Manager
    Se instala Tag Manager

--- resources/views/layouts/layoutOld.blade.php
+++ resources/views/layouts/layoutOld.blade.php
@@ -21,6 +21,17 @@
     <link href="{{ asset('css/cssOld/style.css') }}" rel="stylesheet" />
     <script src="{{ asset('js/jsOld/jquery.min.js') }}" type="text/javascript"></script>
     @production   
+
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5GWTZ55');
+        </script>
+        <!-- End Google Tag Manager -->
+
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174915949-1"></script>
         <script>
@@ -58,6 +69,17 @@
 </head>
 
 <body class="profile-page">
+    
+    @production
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GWTZ55"
+                height="0" width="0" style="display:none;visibility:hidden"
+            ></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+    @endproduction
+    
     <x-navOld />
     <div class="page-header header-filter" data-parallax="true"
         style="background-image: url('{{ asset('img_web/city.jpg') }}');"></div>
