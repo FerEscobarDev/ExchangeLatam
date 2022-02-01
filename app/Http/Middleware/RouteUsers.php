6ad6b3e0655651eed1e68cf213803426b9cbcd5a
keishmen <57463087+keishmen@users.noreply.github.com>
2022-02-01 13:07:10 -0500

    Update RouteUsers.php

--- app/Http/Middleware/RouteUsers.php
+++ app/Http/Middleware/RouteUsers.php
@@ -19,7 +19,7 @@ class RouteUsers
     {
         $route = $request->server->all()['HTTP_HOST'];
 
-        if($route != 'admin.exchangelatam.com' && $route != 'www.admin.exchangelatam.com')
+        if($route != 'admin.tradingautomaticofx.com' && $route != 'www.admin.tradingautomaticofx.com')
         { 
             return $next($request);
         }else
