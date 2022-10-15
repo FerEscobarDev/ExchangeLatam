31b94a584df86b4e77d56dcbacd580ae0194facc
keishmen <57463087+keishmen@users.noreply.github.com>
2022-10-14 21:35:36 -0500

    Update web.php
    corrección en rutas de admin

--- routes/web.php
+++ routes/web.php
@@ -66,8 +66,8 @@ Route::get('/dollarPrices', [DollarPriceController::class, 'indexPublic'])->name
 
 
 //Rutas Admin
-//Route::domain('admin.exchangelatam.com')->middleware('auth', 'verified', 'data')->group(function () {
-Route::domain('admin.exchangelatam.test')->middleware('auth', 'verified', 'data')->group(function () {
+Route::domain('admin.exchangelatam.com')->middleware('auth', 'verified', 'data')->group(function () {
+//Route::domain('admin.exchangelatam.test')->middleware('auth', 'verified', 'data')->group(function () {
 
     //Rutas Notificaciones
     Route::get('/admin/dashboard', [UserController::class, 'admin'])->name('admin.dashboard')->middleware('can:admin.dashboard');/* ok */
