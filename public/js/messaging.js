aa858293b6ae0ea7a66db3df078cf30746c3d687
FerEscobarDev <jeiesc04@outlook.com>
2023-02-06 20:33:28 -0500

    Delete files

--- public/js/messaging.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    var change = setInterval(function() {
-        var content = document.querySelector('#content');
-        var preview = document.querySelector('#preview');
-        preview.innerHTML = content.value;
-    }, 1000);
-});
