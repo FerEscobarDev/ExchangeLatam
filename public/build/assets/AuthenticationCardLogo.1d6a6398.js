2f2455c3cbf0d99ded844b2c6a32272a77b18882
FerEscobarDev <jeiesc04@outlook.com>
2023-03-10 00:07:09 -0500

    Deploy

---
+++ public/build/assets/AuthenticationCardLogo.1d6a6398.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./logoweb.554f3f4d.js";import{o as a,c as e,w as o,f as r,L as s,a as _}from"./app.589eac8f.js";const c=_("img",{src:t},null,-1),u={__name:"AuthenticationCardLogo",setup(n){return(i,m)=>(a(),e(r(s),{href:"/"},{default:o(()=>[c]),_:1}))}};export{u as _};
